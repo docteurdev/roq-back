1e5d202dcc2f1eb39756ed71800ed4f51c9c4c77
docteurdev <77064953+docteurdev@users.noreply.github.com>
2023-01-02 18:34:55 +0100

    param

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -7,8 +7,8 @@ const TypeEmployeModel = require('../models/typeEmploye');
 const RdvModel = require('../models/Rdv');
 
 const sequelize = new Sequelize(process.env.DB_NAME || 'roqya_app', process.env.DB_USER ||'root', process.env.DB_PASSWORD ||'root', {
-    host:process.env.HOST || 'localhost',
-    port:process.env.PORT || 3306,
+    host:process.env.DB_HOST || 'localhost',
+    port:process.env.DB_PORT || 3306,
     dialect: 'mysql',
     // dialectOptions: {
     //   timezone: 'Etc/GMT-2',
