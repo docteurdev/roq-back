161b78d8baf88a5b278c00e4c39a32a3344bf59e
oumar adje <adjeoumar@gmail.com>
2023-05-05 17:23:17 +0200

    fixed db port

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -8,7 +8,7 @@ const RdvModel = require('../models/Rdv');
 
 const sequelize = new Sequelize(process.env.DB_NAME || 'roqya_app', process.env.DB_USER ||'root', process.env.DB_PASSWORD ||'root', {
     host:process.env.DB_HOST || 'localhost',
-    port:process.env.DB_PORT || 3306,
+    port: process.env.NODE_ENV === 'production'? 5860 : 3306,
     dialect: 'mysql',
     // dialectOptions: {
     //   timezone: 'Etc/GMT-2',
